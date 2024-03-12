e2f650f6a486419b4e1abca44d6cbbfa143d6d74
andyzheng679 <andyzheng679@gmail.com>
2024-03-12 12:13:10 -0400

    Finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -14,5 +14,26 @@ public class ReadFour
         {
             // Print out a running total of all the
             // values in the input file.
+
+            System.out.println(runningTotal());
+        }
+
+        public static int runningTotal(){
+            int total = 0;
+
+            try{
+                Scanner fileIn = new Scanner(new File("/Users/andy/Desktop/Projects/FileIOJava/Step4/input.txt"));
+
+                while (fileIn.hasNext()){
+                    int lineIn = fileIn.nextInt();
+                    total += lineIn;
+                }
+                //
+            }catch(IOException e){
+                System.out.println("File not found");
+            }
+
+
+            return total;
         }
 }
